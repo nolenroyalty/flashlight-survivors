4d1e1b6ae5534e26b49f9bbc39b924c180ef9e4b
Nolen <nolen.royalty@gmail.com>
2023-08-10 13:55:14 -0400

    fix light cone's back

--- player/LightCone.gd
+++ player/LightCone.gd
@@ -1,9 +1,28 @@
 extends Area2D
 
+var seconds_per_tick = 0.5
+var damage_per_tick = 1.0
+var bodies = {}
+
+func start_damage_timer(body):
+	var timer = get_tree().create_timer(seconds_per_tick)
+	timer.connect("timeout", self, "damage_if_present", [body])
+
+func damage_if_present(body):
+	if bodies.has(body):
+		print("Dealing %d damage to %s" % [damage_per_tick, body])
+		body.damage(damage_per_tick)
+		start_damage_timer(body)
+
+func on_body_entered(body):
+	bodies[body] = true
+	start_damage_timer(body)
+	print(body)
+
+func on_body_exited(body):
+	bodies.erase(body)
+	print(body)
+
 func _ready():
-	print($Points.polygon)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var _ignore = connect("body_entered", self, "on_body_entered")
+	_ignore = connect("body_exited", self, "on_body_exited") 
