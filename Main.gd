99f705ab5cd5ed0211a839cba944ed6e5fb4f7e7
Nolen Royalty <nolen@deepfield.net>
2023-08-17 15:08:45 -0400

    delete debug statements, add door spawn manager

--- Main.gd
+++ Main.gd
@@ -35,11 +35,5 @@ func _process(_delta):
 	if Input.is_action_just_pressed("debug_spawn_enemy"):
 		var enemy = Enemy.instance()
 		add_child(enemy)
-	if Input.is_action_just_pressed("debug_spawn_door"):
-		# We should avoid overlapping doors
-		var door = Door.instance()
-		var x = rng.randi_range(50, 400)
-		var y = rng.randi_range(50, 300)
-		door.position = Vector2(x, y)
-		add_child(door)
+
 
